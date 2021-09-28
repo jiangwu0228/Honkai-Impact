18548aad2a6267329543aff934b9b19067cdb395
kasitunagogogo <napianhai6631@gmail.com>
2021-09-29 10:51:04 +1300

    23-331. Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image="https://uploadstatic-sea.mihoyo.com/contentweb/20200814/2020081419354392790.png"
+      title="Kiana·Kaslana"
+      address="7/12/1998"
+      bio="这就是……我的承诺。"
+    />
+  );
+}
+
+export default MeetupDetails;
